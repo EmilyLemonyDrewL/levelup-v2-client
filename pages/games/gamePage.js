22588a74f2796585d1722d0bb634c8577fc8bd17
Emily Drew L <DontDenyHighFives@gmail.com>
2024-05-30 17:13:18 -0500

    chapter 11 finished, 12 almost done

--- pages/games/gamePage.js
+++ pages/games/gamePage.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useRouter } from 'next/router';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../utils/data/gameData';
+import { useAuth } from '../../utils/context/authContext';
 
 function Home() {
   const [games, setGames] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
 
   useEffect(() => {
     getGames().then((data) => setGames(data));
   }, []);
+  const showGames = () => {
+    getGames(user.uid).then((data) => setGames(data));
+  };
 
   return (
     <article className="games">
+      <Button
+        onClick={() => {
+          router.push('/games/new');
+        }}
+      >
+        New Game
+      </Button>
       <h1>Games</h1>
       {games.map((game) => (
         <section key={`game--${game.id}`} className="game">
-          <GameCard title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} />
+          <GameCard title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} id={game.id} onUpdate={showGames} />
         </section>
       ))}
     </article>
